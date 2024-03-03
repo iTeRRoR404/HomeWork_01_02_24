9f869e300a0c14106d47792ad4eee5b19c5dc569
iTeRRoR404 <sbuzin@bk.ru>
2024-03-03 19:50:41 +0300

    Initial commit

---
+++ src/test/java/NewWindowTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class NewWindowTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testNewWindow() {
+
+        driver.get("https://the-internet.herokuapp.com/windows");
+        driver.findElement(By.linkText("Click Here")).click();
+
+        for (String tab : driver.getWindowHandles()) {
+            driver.switchTo().window(tab);
+        }
+
+        String actualText = driver.findElement(By.tagName("h3")).getText();
+        String expectedText = "New Window";
+        Assert.assertEquals(actualText, expectedText);
+        assert actualText.contains("New Window");
+    }
+}
